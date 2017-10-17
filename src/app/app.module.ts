982deaad8d7cee568627bc770e7d5eb98579e0d1
AitBenOm <omar.benaissa@outlook.com>
2017-10-17 10:48:26 +0200

    13/10/2017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import {AppRoutingModule} from "./app-routing-module";
 import {AuthGuardService} from "./auth-guard.service";
 import {AuthService} from "./auth.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard";
 
 
 
@@ -35,7 +36,7 @@ import {AuthService} from "./auth.service";
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthGuardService, AuthService],
+  providers: [ServersService, AuthGuardService, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {CanComponenetDeactivate} from "./can-deactivate-guard";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponenetDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit= false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     //console.log(this.route.snapshot.queryParams);
@@ -22,7 +25,9 @@ export class EditServerComponent implements OnInit {
       (query: Params) => {
     this.allowEdit = +query['allowEdit'] === 1 ? true : false ;
       }
-    )
+    );
+    this.route.fragment.subscribe();
+    const id = +this.route.snapshot.params['id'];
    // console.log(this.route.snapshot.fragment);
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
@@ -31,6 +36,18 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route} );
   }
 
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean{
+    if( !this.allowEdit){
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved){
+      return confirm('Do you want to discard tha changes ?');
+    }else {
+      return true;
+    }
+  }
 }
